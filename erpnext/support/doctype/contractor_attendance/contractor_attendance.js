f726cc8d2dc60d6219507e7ae1cf85aae3610524
ramprabusv <ramprabusv@agnikul.in>
2023-10-26 22:21:40 +0530

    scanner

--- erpnext/support/doctype/contractor_attendance/contractor_attendance.js
+++ erpnext/support/doctype/contractor_attendance/contractor_attendance.js
@@ -1,8 +1,53 @@
-// Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
-// For license information, please see license.txt
+// // Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
+// // For license information, please see license.txt
 
-// frappe.ui.form.on("Contractor Attendance", {
-// 	refresh(frm) {
+frappe.ui.form.on('Contractor Attendance', {
+    refresh(frm) {
+        if (frm.doc.__islocal) {
+            // frappe.msgprint("Hellooo");
+            new frappe.ui.Scanner({
+                dialog: true, // open camera scanner in a dialog
+                multiple: false, // stop after scanning one value
+                on_scan(data) {
+                    var nameWorker = data.decodedText;
 
-// 	},
-// });
+                    frm.call({
+                        doc: frm.doc,
+                        method: 'frm_call',
+                        args: {
+                            name_worker: nameWorker,
+                        },
+                        callback: function (r) {
+
+                            frappe.msgprint(r.message)
+                            if (r.message == 'Entry') {
+                                // window.location.href = 'http://127.0.0.1:8000/app/contractor-attendance';
+                                window.location.href = 'http://127.0.0.1:8000/agkl-maintenance';
+
+                            }
+                            else if (r.message == 'Exit') {
+                                // window.location.href = 'http://127.0.0.1:8000/app/contractor-attendance';
+                                // window.location.href = 'http://127.0.0.1:8000/app/contractor-attendance/new-contractor-attendance-xbfxtaidgg';
+                                window.location.href = 'http://127.0.0.1:8000/agkl-maintenance';
+
+                            }
+                            else {
+                                // window.location.href = 'http://127.0.0.1:8000/app/contractor-attendance';
+                                // window.location.href = 'http://127.0.0.1:8000/app/contractor-attendance/new-contractor-attendance-xbfxtaidgg';
+                                window.location.href = 'http://127.0.0.1:8000/agkl-maintenance';
+
+                            }
+
+
+                        }
+                    });
+
+                }
+            });
+
+        }
+    },
+
+});
+
+
--- erpnext/support/doctype/contractor_attendance/contractor_attendance.py
+++ erpnext/support/doctype/contractor_attendance/contractor_attendance.py
@@ -1,9 +1,30 @@
 # Copyright (c) 2023, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ContractorAttendance(Document):
-	pass
+	# pass
+	@frappe.whitelist()
+	def frm_call(self,name_worker):
+		
+		if(frappe.db.exists("Contractors",name_worker)):
+			if (frappe.db.exists("Contractor Attendance",name_worker+'-'+frappe.utils.nowdate())):				
+				doc = frappe.get_doc("Contractor Attendance", name_worker+'-'+frappe.utils.nowdate())
+				if doc.out_time==None:
+					doc.out_time=frappe.utils.now().split()[1][:5]
+				doc.save()
+				return "Exit"
+			else:
+				doc = frappe.new_doc("Contractor Attendance")
+				doc.worker_code=name_worker
+				if doc.in_time==None:
+					doc.in_time=frappe.utils.now().split()[1][:5]
+				doc.insert()
+				return "Entry"
+		else:
+			return "Nope"
+
+
